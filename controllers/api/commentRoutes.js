ef3f21cc0ea2909dff899adb64e8a910ac8a44ca
Keona Kirby <keonakirby@gmail.com>
2021-04-27 21:38:20 -0500

    minor changes to the post display

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,6 +2,26 @@ const router = require('express').Router();
 const { Comment} = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    
+    const commentData = await Comment.findAll();
+
+    
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+    
+    res.render('comment', { 
+      comments, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 router.post('/', withAuth, async (req, res) => {
     try {
       const newComment = await Comment.create({
@@ -9,7 +29,7 @@ router.post('/', withAuth, async (req, res) => {
         user_id: req.session.user_id,
       });
   
-      res.status(200).json(newComment);
+      res.redirect('/')
     } catch (err) {
       res.status(400).json(err);
     }
