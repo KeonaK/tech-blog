fcee8c0eead559e470f9b23ee0326b9e6068dfe3
Keona Kirby <keonakirby@gmail.com>
2021-04-28 00:01:06 -0500

    Added/readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # tech-blog
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+This project is a CMS style blog where developers can publish their blog posts and comment on other developers’ posts as well.
+
+## Table of contents
+
+- [Description](#description)
+- [ScreenShot](#screenshot)
+- [Heroku](#Heroku)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## ScreenShot
+
+![screen shot]()
+
+## Heroku
+
+https://git.heroku.com/tech-blog-project.git
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+npm install or npm i after you have cloned the repository
+
+## Usage
+
+npm start
+
+## License
+
+This project is licensed under the MIT license.
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at keonakirby@gmail.com. You can find more of my work at https://github.com/Keonak
--- controllers/api/createRoutes.js
+++ controllers/api/createRoutes.js
@@ -2,6 +2,32 @@ const router = require('express').Router();
 const { Create } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+
+//get post
+router.get('/creates/:id', async (req, res) => {
+  try {
+    const createData = await Create.findByPk(req.params.id, {
+      where: [
+        {
+          model: User,
+          attributes: ["name"]
+        },
+      ],
+    });
+
+    const create = createData.get({ plain: true });
+
+    res.render('create', {
+      //grabs all the properties that are in create
+      ...create,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//create post 
 router.post('/', withAuth, async (req, res) => {
   try {
     const newCreate = await Create.create({
@@ -15,6 +41,22 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+//update post 
+router.put('/creates/:id', withAuth, async (req, res) => {
+  try {
+    const updateCreate = await Create.create({
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    res.status(200).json(updateCreate);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//delete post 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const CreateData = await Create.destroy({
