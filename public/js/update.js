1ad1cdb2c416e8a47bfcdfcb59ba1ac69d1e38a3
Keona Kirby <keonakirby@gmail.com>
2021-04-28 00:36:03 -0500

    Added/update.js file started

---
+++ public/js/update.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document
+  .querySelector('.new-create-form')
+  .addEventListener('click', newFormHandler);
+
+document
+  .querySelector('.project-list')
+  .addEventListener('click', delButtonHandler);
